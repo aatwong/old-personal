ac61e458a9ad68fab3991ef9f3604c1716b38597
andrewatwong <andrewatwong@gmail.com>
2016-08-27 22:18:19 -0700

    added external script.js file and deleted javascript function from index.html

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+//Returns the appropriate greeting based on time of day
+function Greet() {
+  var date = new Date();
+  var hour = date.getHours();
+  if (hour >= 18) {
+    greeting = "Good evening";
+  } else if (hour >= 12) {
+    greeting = "Good afternoon";
+  } else if (hour >= 3) {
+    greeting = "Good morning";
+  } else {
+    greeting = "Hello";
+  }
+  document.write(greeting);
+}
